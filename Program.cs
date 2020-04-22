7a2b53a5f1fef722b79692aca9e595c8f04f05c2
Ilya Abramovich <ilya>
2020-04-22 20:14:51 +0300

    Added important Enter

--- Program.cs
+++ Program.cs
@@ -123,6 +123,7 @@ namespace FirstOOPInheritance
 
                 }
                 Console.WriteLine();
+                Console.WriteLine();
             }
             Console.ReadKey();
         }
